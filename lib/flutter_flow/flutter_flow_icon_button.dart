18939c4db03e21cfb4866ad968060cee9a9befc8
Алексей Кузьмин <kuzmin_a@liman-trade.com>
2026-06-03 20:27:58 +0300

    Совместимость с Flutter 3.44.1: font_awesome_flutter 10.7.0→11.0.0, page_transition 2.1.0→2.2.0. Миграция FaIcon: FaIcon(IconData) заменён на Icon(IconData), _updateIcon() упрощён (используется widget.icon напрямую).

--- lib/flutter_flow/flutter_flow_icon_button.dart
+++ lib/flutter_flow/flutter_flow_icon_button.dart
@@ -60,21 +60,14 @@ class _FlutterFlowIconButtonState extends State<FlutterFlowIconButton> {
   }
 
   void _updateIcon() {
-    final isFontAwesome = widget.icon is FaIcon;
-    if (isFontAwesome) {
+    if (widget.icon is FaIcon) {
       FaIcon icon = widget.icon as FaIcon;
-      effectiveIcon = FaIcon(
-        icon.icon,
-        size: icon.size,
-      );
+      effectiveIcon = widget.icon;
       iconSize = icon.size;
       iconColor = icon.color;
-    } else {
+    } else if (widget.icon is Icon) {
       Icon icon = widget.icon as Icon;
-      effectiveIcon = Icon(
-        icon.icon,
-        size: icon.size,
-      );
+      effectiveIcon = widget.icon;
       iconSize = icon.size;
       iconColor = icon.color;
     }
